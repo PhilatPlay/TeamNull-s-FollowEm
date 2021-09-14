2ad84b56b4fe778b15fc031c004c8d6da45dc50d
ChaseJ-01 <chase.jones@revature.net>
2021-09-14 12:37:43 -0400

    Added blank Angular project

--- FollowEm-FrontEnd/src/app/app.component.spec.ts
+++ FollowEm-FrontEnd/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testapp'`, () => {
+  it(`should have as title 'FollowEm-FrontEnd'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('testapp');
+    expect(app.title).toEqual('FollowEm-FrontEnd');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('FollowEm-FrontEnd app is running!');
   });
 });
