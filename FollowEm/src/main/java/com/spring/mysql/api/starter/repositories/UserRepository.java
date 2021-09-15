d8feca65139d3a6b4cc6ef5d3d70ddc4aad15428
PhilatPlay <philip.polo@yahoo.com>
2021-09-15 10:00:03 -0500

    Update UserRepository.java
    Added Getting needed User data Method

--- FollowEm/src/main/java/com/spring/mysql/api/starter/repositories/UserRepository.java
+++ FollowEm/src/main/java/com/spring/mysql/api/starter/repositories/UserRepository.java
@@ -6,5 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+  // getting those the user is following
+    @Query(value = "SELECT * FROM users inner join posts using (user_id) WHERE user_id = ?1", nativeQuery = true)
+    public List<Object> getUserData(Long user_id);
 }
