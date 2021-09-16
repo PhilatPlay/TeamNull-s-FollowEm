9b788dde85b78dddc7c148985e2b0c969fec6049
PhilatPlay <philip.polo@yahoo.com>
2021-09-16 14:31:16 -0500

    Update CorsConfiguration.java
    added needed semicolon

--- FollowEm/src/main/java/com/spring/mysql/api/starter/CorsConfiguration.java
+++ FollowEm/src/main/java/com/spring/mysql/api/starter/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package com.spring.mysql.api.starter
+package com.spring.mysql.api.starter;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
