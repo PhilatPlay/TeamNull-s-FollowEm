acff45a4b0c4f5c7aaed0017a1ab3f6b74b4512e
PhilatPlay <philip.polo@yahoo.com>
2021-09-20 00:34:45 -0500

    Update FollowRepository.java
    follows and followed queries improvement added

--- FollowEm/src/main/java/com/spring/mysql/api/starter/repositories/FollowRepository.java
+++ FollowEm/src/main/java/com/spring/mysql/api/starter/repositories/FollowRepository.java
@@ -11,11 +11,11 @@ import java.util.List;
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
 // getting id's of those following the user
-    @Query(value = "SELECT followed_id FROM follow_em.follows WHERE following_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT followed_id FROM follow_em.follows WHERE status = 'approved' AND following_id = ?1", nativeQuery = true)
     public List<Long> getIdsOfThoseIFollow(Long following_id);
 
     // getting ids of those the user is following
-    @Query(value = "SELECT following_id FROM follow_em.follows WHERE followed_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT following_id FROM follow_em.follows WHERE status = 'approved' AND followed_id = ?1", nativeQuery = true)
    public List<Long> getIdsOfThoseThatFollowMe(Long followed_id);
 
 
