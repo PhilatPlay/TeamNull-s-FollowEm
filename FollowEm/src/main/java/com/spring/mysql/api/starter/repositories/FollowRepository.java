f0c218f688ef25d59408ec622984f40101b73d5f
PhilatPlay <philip.polo@yahoo.com>
2021-09-20 02:04:42 -0500

    Update FollowRepository.java
    Added functionality to get ids of those a user's blocked.

--- FollowEm/src/main/java/com/spring/mysql/api/starter/repositories/FollowRepository.java
+++ FollowEm/src/main/java/com/spring/mysql/api/starter/repositories/FollowRepository.java
@@ -14,6 +14,10 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
     // getting ids of those the user is following
     @Query(value = "SELECT following_id FROM follow_em.follows WHERE status = 'approved' AND followed_id = ?1", nativeQuery = true)
    public List<Long> getIdsOfThoseThatFollowMe(Long followed_id);
+    
+    // getting id's of those a user blocked
+    @Query(value = "SELECT following_id FROM follow_em.follows WHERE status = 'decline' AND followed_id = ?1", nativeQuery = true)
+    public List<Long> getIdsOfThoseIBlocked(Long followed_id);
 
 
 }
